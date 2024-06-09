7e290319d0c8f1a224d5de34d1794327a0695cfc
Dembel02 <IRGilyazov@yandex.ru>
2024-06-09 22:01:15 +0500

    Ищет числа которые делятся на 3 и суммируются

--- 4.8.8_DZ.py
+++ 4.8.8_DZ.py
@@ -5,10 +5,13 @@ url = 'https://parsinger.ru/4.8/7/index.html'
 
 response = requests.get(url=url)
 response.encoding = 'utf-8'
-
+result = []
 soup = BeautifulSoup(response.text, 'html.parser')
 table = soup.find_all('tr')
 for i in table:
-    table_i = i.find_all('tr')
+    table_i = i.find_all('td')
     for i in table_i:
-        print(i)
+        j = int(i.text)
+        if j % 3 == 0:
+            result.append(j)
+print(sum(result))
