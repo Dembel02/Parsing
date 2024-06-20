43969e38a21580ad53b6d2efcdcdf153220b22ac
Dembel02 <IRGilyazov@yandex.ru>
2024-06-20 17:14:23 +0500

    Исправил ошибку записи значении в каждую строку забыл отступ

--- 4.9.2.py
+++ 4.9.2.py
@@ -6,12 +6,11 @@ from bs4 import BeautifulSoup
 with open('res.csv', 'w', encoding='utf-8-sig', newline='') as file:
     writer = csv.writer(file, delimiter=';')
     writer.writerow([
-        'Наименование', 'Цена', 'Бренд', 'Тип', 'Подключение',
-        'Игровая'])
+        'Наименование', 'Цена', 'Бренд', 'Тип', 'Подключение', 'Игровая'])
 # 1 ------------------------------------------------------------
 
 # 2 ------------------------------------------------------------
-url = 'https://parsinger.ru/html/index3_page_2.html'
+url = 'http://parsinger.ru/html/index3_page_2.html'
 
 response = requests.get(url=url)
 response.encoding = 'utf-8'
@@ -23,12 +22,12 @@ soup = BeautifulSoup(response.text, 'lxml')
 name = [x.text.strip() for x in soup.find_all('a', class_ = 'name_item')]
 
 # Извлекаем описание товаров и разбиваем на строки
-description = [x.text.split('\n') for x in soup.find_all('div',
-class_ = 'description')]
+description = [x.text.split('\n') for x in soup.find_all('div', class_ = 'description')]
 
 # Извлекаем цену товаров
 price = [x.text for x in soup.find_all('p', class_ = 'price')]
 # 3 ------------------------------------------------------------
+
 
 # 4 ------------------------------------------------------------
 # Открываем файл для дополнительной записи данных
@@ -38,5 +37,6 @@ with open('res.csv', 'a', encoding='utf-8-sig', newline='') as file:
 
         # формируем строку для записи
         flatten = item, price, *[x.split(':')[1].strip() for x in descr if x]
-    writer.writerow(flatten)
+        writer.writerow(flatten)
+
 print('Файл res.csv создан')
