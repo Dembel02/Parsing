d15837678e648fcc27b57f2c383fe2c823d6d05a
Dembel02 <IRGilyazov@yandex.ru>
2024-07-22 23:07:22 +0500

    Исправил код, теперь выгружаются в файл csv данные по всем четырем страницам, только необходимо исправить порядок столбцов

--- 4.9.4_DZ.py
+++ 4.9.4_DZ.py
@@ -11,32 +11,40 @@ with open('res.csv', 'w', encoding='utf-8-sig', newline='') as file:
 # 1 ------------------------------------------------------------
 
 # 2 ------------------------------------------------------------
+
 url ='https://parsinger.ru/html/index4_page_1.html'
 
 response = requests.get(url=url)
 response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'lxml')
 # 2 ------------------------------------------------------------
-
+pagen = int(soup.find('div', 'pagen').find_all('a')[-1].text)
+print(pagen)
+for i in range (1, pagen+1):
+    url = f'https://parsinger.ru/html/index4_page_{i}.html'
+    response = requests.get(url=url)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'lxml')
+    print(url)
 # 3 ------------------------------------------------------------
 # Извлекаем имена товаров и убираем лишние пробелы
-name = [x.text.strip() for x in soup.find_all('a', class_ = 'name_item')]
+    name = [x.text.strip() for x in soup.find_all('a', class_ = 'name_item')]
 
 # Извлекаем описания товаров и разбиваем на строки
-description = [x.text.split('\n') for x in soup.find_all('div', class_ = 'description')]
+    description = [x.text.split('\n') for x in soup.find_all('div', class_ = 'description')]
 
 # Извлекаем цены товаров
-price = [x.text for x in soup.find_all('p', class_ = 'price')]
+    price = [x.text for x in soup.find_all('p', class_ = 'price')]
 # 3 ------------------------------------------------------------
 
 # 4 ------------------------------------------------------------
 # Открываем файл для дополнительной записи данных в таблицу
-with open('res.csv', 'a', encoding='utf-8-sig', newline='') as file:
-    writer = csv.writer(file, delimiter=';')
-    for item, price, descr in zip(name, price, description):
-        
-        # Формируем строку для записи
-        flatten = item, price, *[x.split(':')[1].strip() for x in descr if x]
-        writer.writerow(flatten)
+    with open('res.csv', 'a', encoding='utf-8-sig', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        for item, price, descr in zip(name, price, description):
+            
+            # Формируем строку для записи
+            flatten = item, price, *[x.split(':')[1].strip() for x in descr if x]
+            writer.writerow(flatten)
 
 print('Файл res.csv создан')
