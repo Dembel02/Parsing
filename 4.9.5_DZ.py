1316be0a7bb9075dd06c186b9b841071f8ad2d77
Dembel02 <IRGilyazov@yandex.ru>
2024-07-30 23:04:24 +0500

    Дописал код парсинга данных с карточки объекта, кроме ссылки, на сегодня время вышло, завтра надеюсь продолжить

--- 4.9.5_DZ.py
+++ 4.9.5_DZ.py
@@ -26,9 +26,24 @@ soup = BeautifulSoup(response.text, 'lxml')
 # 4 -------------------------------------------------------------------
 pagen = int(soup.find('div', class_ = 'pagen').find_all('a')[-1].text)
 # В переменную pagen спарсим количесто страниц 
-for i in range(1, pagen + 1):
-    url = f'https://parsinger.ru/html/index1_page_{i}.html'
-    response = requests.get(url=url)
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'lxml')
-    print(url)
+# for i in range(1, pagen + 1):
+#     url = f'https://parsinger.ru/html/index1_page_{i}.html'
+#     response = requests.get(url=url)
+#     response.encoding = 'utf-8'
+#     soup = BeautifulSoup(response.text, 'lxml')
+#     print(url)
+item_card = soup.find_all('a').get('href')
+for i in item_card:
+    print(i)
+url = 'https://parsinger.ru/html/watch/1/1_1.html'
+response_1 = requests.get(url=url)
+response_1.encoding = 'utf-8'
+soup = BeautifulSoup(response_1.text, 'lxml')
+
+name = soup.find('p', id = 'p_header').text
+article = soup.find('p', class_ = 'article').text.split()[1]
+description = [x.text.split('\n') for x in soup.find('ul', id = 'description')]
+in_stock = int(soup.find('span', id = 'in_stock').text.split(':')[1])
+price = soup.find('span', id = 'price').text.split()[0]
+old_price = soup.find('span', id = 'old_price').text.split()[0]
+# print(old_price)
