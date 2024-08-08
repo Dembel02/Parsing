6a1db5b03d4c81497a7ba83ff325cbeaa00d2bc4
Dembel02 <IRGilyazov@yandex.ru>
2024-08-08 23:01:43 +0500

    Парсер данных со 160 карт товаров начало

--- 4.9.6.DZ.py
+++ 4.9.6.DZ.py
@@ -5,6 +5,20 @@ import requests
 # 1 -------------------------------------------------------------
 
 # 2 -------------------------------------------------------------
-with open ('rez_1.csv', w, encoding='utf-8-sig', newline='') as file:
+with open ('res_1.csv', 'w', encoding='utf-8-sig', newline='') as file:
     writer = csv.writer(file, delimiter=';')
-    
+    # writer.writerow('')
+# 2 -------------------------------------------------------------
+
+# 3 -------------------------------------------------------------
+url = 'https://parsinger.ru/html/index1_page_1.html'
+
+response = requests.get(url=url)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+
+# 3 -------------------------------------------------------------
+
+# 4 -------------------------------------------------------------
+pagen = soup.find('div', class_ = 'pagen').find_all('a')[-1].text
+print(pagen)
