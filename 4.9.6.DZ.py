60bf6c2ce408c6d44594c95935afa6cbf7a04939
Dembel02 <IRGilyazov@yandex.ru>
2024-08-15 21:19:33 +0500

    вытащил наименование и характеристики по одному типу товаров

--- 4.9.6.DZ.py
+++ 4.9.6.DZ.py
@@ -20,19 +20,17 @@ soup = BeautifulSoup(response.text, 'lxml')
 # 3 -------------------------------------------------------------
 
 # 4 -------------------------------------------------------------
-# pagen = int(soup.find('div', class_ = 'pagen').find_all('a')[-1].text)
-# for i in range(1, pagen + 1):
-#     url = f'https://parsinger.ru/html/index1_page_{i}.html'
-#     response = requests.get(url=url)
-#     response.encoding = 'utf-8'
-#     soup = BeautifulSoup(response.text, 'lxml')
-#     name = soup.find_all('a', class_ = 'name_item')
-#     for i in name:
-#         # j = i.find_all('a', class_ = 'name_item')
-#         print(i.find('a').text)
-    
-#     print(name)
-name = soup.find_all('a', class_ = 'name_item')
-for i in name:
-    i.find('a', [f'name_{j}' for j in range(1, 8)])
-    print(i.find('a'))
+pagen = int(soup.find('div', class_ = 'pagen').find_all('a')[-1].text)
+for i in range(1, pagen + 1):
+    url = f'https://parsinger.ru/html/index1_page_{i}.html'
+    response = requests.get(url=url)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'lxml')
+    names = soup.find_all('a', class_ = 'name_item')
+    for i in names:
+        name = i.text
+        # print(name)
+        name_ = [x.text for x in soup.find_all('a', class_ = 'name_item')]
+        description_list = [x.text.split('\n') for x in soup.find('div', class_ = 'description')]
+        description = [item for sublist in description_list for item in sublist]
+        print(name, *[x.split(':')[1].strip() for x in description if ':' in x])
