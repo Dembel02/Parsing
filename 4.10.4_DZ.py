56a6288af25e17d7d54523e0c439a4b57870aa33
Dembel02 <IRGilyazov@yandex.ru>
2024-08-25 22:47:35 +0500

    Задача решена

--- 4.10.4_DZ.py
+++ 4.10.4_DZ.py
@@ -16,3 +16,28 @@ url = 'https://parsinger.ru/html/index4_page_1.html'
 response = requests.get(url=url)
 response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'lxml')
+
+name = [x.text.strip() for x in soup.find_all('a', class_='name_item')]
+description = [x.text.strip().split('\n') for x in soup.find_all('div', class_='description')]
+price = [x.text for x in soup.find_all('p', class_='price')]
+
+result_json = []
+
+for list_item, price_item, name in zip(description, price, name):
+    result_json.append({
+        'Наименование': name,
+        'Бренд': [x.split(':')[1].strip() for x in list_item][0],
+        'Форм-фактор': [x.split(':')[1].strip() for x in list_item][1],
+        'Ёмкость': [x.split(':')[1].strip() for x in list_item][2],
+        'Объем буферной памяти': [x.split(':')[1].strip() for x in list_item][3],
+        'Цена': price_item
+
+    })
+
+# 3 ------------------------------------------------------
+
+# 4 ------------------------------------------------------
+with open('res_1.json', 'w', encoding='utf-8') as file:
+    json.dump(result_json, file, indent=4, ensure_ascii=False)
+# 4 ------------------------------------------------------
+print(description)
