87990a0265ca4fb698ba905b2f7f99ba15713f86
Dembel02 <IRGilyazov@yandex.ru>
2024-08-27 00:13:48 +0500

    1

--- 4.10.5_DZ.py
+++ 4.10.5_DZ.py
@@ -20,4 +20,8 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = 
+url = 'https://parsinger.ru/html/index1_page_1.html'
+response = requests.get(url=url)
+response.encoding = 'utf-8'
+soup = BeautifulSoup(response.text, 'lxml')
+print(soup)
