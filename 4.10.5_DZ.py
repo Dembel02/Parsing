6bedcd3b5d01d57a09cf4734b8d16123f5b19ef6
Dembel02 <IRGilyazov@yandex.ru>
2024-08-27 21:31:09 +0500

    Пока парсинг по одной странице, завтра доделаем по всем страницам и по всем типам

--- 4.10.5_DZ.py
+++ 4.10.5_DZ.py
@@ -24,4 +24,24 @@ url = 'https://parsinger.ru/html/index1_page_1.html'
 response = requests.get(url=url)
 response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'lxml')
-print(soup)
+
+name = [x.text.strip() for x in soup.find_all('a', class_ = 'name_item')]
+description = [x.text.strip().split('\n') for x in soup.find_all('div', class_ = 'description')]
+price = [x.text.strip() for x in soup.find_all('p', class_ = 'price')]
+
+result_json = []
+
+for list_item, price_item, name in zip (description, price, name):
+    result_json.append({
+        "Наименование": name,
+        "Бренд": [x.split(':')[1].strip() for x in list_item][0],
+        "Тип подключения": [x.split(':')[1].strip() for x in list_item][1],
+        "Цвет": [x.split(':')[1].strip() for x in list_item][2],
+        "Тип наушников": [x.split(':')[1].strip() for x in list_item][3],
+        "Цена": price_item
+    })
+
+with open('res_4.10.5.json', 'w', encoding= 'UTF-8') as file:
+    json.dump(result_json, file, indent=4, ensure_ascii=False)
+
+print(result_json)
