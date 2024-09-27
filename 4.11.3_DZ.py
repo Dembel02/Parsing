1ed95ac2fca49cab7d119c70418aeb9354fbd181
Dembel02 <IRGilyazov@yandex.ru>
2024-09-28 00:10:31 +0500

    test

--- 4.11.3_DZ.py
+++ 4.11.3_DZ.py
@@ -3,5 +3,21 @@ import requests
 url = 'https://parsinger.ru/downloads/get_json/res.json'
 
 response = requests.get(url=url).json()
+watch = 0
+mobile = 0
+mouse = 0
+hdd = 0
+headphones = 0
+
 for item in response:
-    print(item["categories"])
+    if item["categories"] == 'watch':
+        watch += 1
+    elif item["categories"] == 'mobile':
+        mobile += 1
+    elif item["categories"] == 'mouse':
+        mouse += 1
+    elif item["categories"] == 'hdd':
+        hdd += 1
+    elif item["categories"] == 'headphones':
+        headphones += 1
+print(watch)
