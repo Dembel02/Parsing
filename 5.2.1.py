0a2d6b5d00596b955091e072cbc0c2d991b3d826
Dembel02 <IRGilyazov@yandex.ru>
2024-10-04 18:19:50 +0500

    пробный пуск selenium

--- 5.2.1.py
+++ 5.2.1.py
@@ -1,31 +1,7 @@
-# import time
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-
-# with webdriver.Chrome(ChromeDriverManager().install()) as driver:
-#     driver.get("https://stepik.org/course/104774")
-#     time.sleep(5)
-
-# Или
-
 import time
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
-from selenium.webdriver.chrome.service import Service as ChromiumService
 
-with webdriver.Chrome(
-        service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())) as driver:
-    driver.get("https://stepik.org/course/104774")
-    time.sleep(5)
-
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium import webdriver
-
-# # Initialize the ChromeDriver with the installed executable
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-
-# # You can use the driver now
-# with driver:
-#     # Your code here
-#     driver.get("https://www.google.com")
+url = 'https://stepik.org/course/104774'
+browser = webdriver.Chrome()
+browser.get(url)
+time.sleep(5)
