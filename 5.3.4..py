b73b94f54a39e8eec0e53757557a4406e10200fe
Dembel02 <IRGilyazov@yandex.ru>
2024-11-03 21:48:17 +0500

    проверка

--- 5.3.4..py
+++ 5.3.4..py
@@ -1,4 +1,22 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+
+options_chrome = webdriver.ChromeOptions()
+options_chrome.add_argument('--headless')
+options_chrome.add_extension('coordinates.crx')
+
+
+
+
+with webdriver.Chrome(options=options_chrome) as browser:
+    url = 'https://yandex.ru/'
+    browser.get(url)
+    time.sleep(5)
+    a = browser.find_element(By.TAG_NAME, 'a')
+    print(a.get_attribute('href'))
