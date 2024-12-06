9a1f1987299957fae772968ab7adc421d6b0d855
Dembel02 <IRGilyazov@yandex.ru>
2024-12-06 23:18:18 +0500

    1

---
+++ 5.3.5.py
@@ -0,0 +1,9 @@
+import time
+from selenium import webdriver
+
+options_chrome = webdriver.ChromeOptions()
+options_chrome.add_argument('user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data')
+with webdriver.Chrome(options=options_chrome) as browser:
+    url = 'https://yandex.ru/'
+    browser.get(url)
+    time.sleep(10)
