c149042f53b7088f3a89a4538965e36b6b741d00
Dembel02 <IRGilyazov@yandex.ru>
2024-12-15 21:38:42 +0500

    1

--- 5.3.6.py
+++ 5.3.6.py
@@ -1,10 +1,25 @@
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+
+# url = 'https://2ip.ru/'
+# with webdriver.Chrome() as browser:
+#     browser.get(url)
+#     time.sleep(5)
+#     print(browser.find_element(By.ID, 'd_clip_button').find_element(By.TAG_NAME, 'span').text)
+#     time.sleep(5)
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+proxy = '8.210.83.33:80'
 url = 'https://2ip.ru/'
-with webdriver.Chrome() as browser:
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--proxy-server=%s' % proxy)
+
+with webdriver.Chrome(options=chrome_options) as browser:
     browser.get(url)
-    time.sleep(5)
     print(browser.find_element(By.ID, 'd_clip_button').find_element(By.TAG_NAME, 'span').text)
     time.sleep(5)
